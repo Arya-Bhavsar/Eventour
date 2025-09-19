0d053f72f8053742f236bd4adee4016eeec41cf0
Jonathan Pater <179418214+JonathanPater03@users.noreply.github.com>
2025-09-19 13:12:49 -0400

    date and location input MVP connection with backend

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import DateLocationInput from './components/dateLocationInput';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <DateLocationInput />
     </div>
   );
 }
