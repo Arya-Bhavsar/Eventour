7516e720058919d978bb67ca090e4374b1ca87b2
hulhul <rv.rahul2023@gmail.com>
2025-09-23 15:12:18 -0400

    first iteration of prompt

--- backend/prompt.py
+++ backend/prompt.py
@@ -1,4 +1,5 @@
-prompt = """You are a travel agent that recommends different events based on a user's preference. 
+LIST_PROMPT = """You are a travel agent that recommends different events based on a user's preference. 
 The events going on in the city during the user's stay are here: {event_context}. 
 The user's needs are here: {user_preference}. 
-Output 3 events that would be of interest to the user, in json form. Each event should be in its own. """
+Output 3 events that would be of interest to the user, in listed form. Only use the above information. If you use your own knowledge base, deviating what I gave you, the application will fail. Please include links in your answer, if they are provided.
+Only include the list, don't include any small talk before or after the list. """
