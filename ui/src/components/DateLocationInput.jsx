256861969d23ac99aac3ab73402f660887120879
Jonathan Pater <179418214+JonathanPater03@users.noreply.github.com>
2025-09-25 14:22:21 -0400

    fixed dateLocationInput css error

--- ui/src/components/DateLocationInput.jsx
+++ ui/src/components/DateLocationInput.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './DateLocationInput.css'
+import './dateLocationInput.css'
 
 export default function DateLocationInput() {
     // States for location, start date, end date
